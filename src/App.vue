30f280e5a6ff3a80641140fcd632fc59835fdc8f
davytan07 <45758052+davytan07@users.noreply.github.com>
2024-03-22 10:04:27 +0800

    Added HealthGuru Icon + Placeholder Home and About View
    - Changed default Vue icon to HealthGuru icon
    - Changed background colours

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+    </div>
+    <router-view/>
+  </template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #929EAF;
+}
+
+#nav a.router-link-exact-active {
+  color: white;
+}
+</style>
