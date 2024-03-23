51ebc8164e16c3f6cd7498c667943dc9cc95eca4
Kevin Choy <kevinchoy111@gmail.com>
2024-03-23 16:50:45 +0800

    Added weight chart widget
    Added weight chart widget on the homepage with hard coded data points first

---
+++ src/components/Weight.vue
@@ -0,0 +1,77 @@
+<template>
+    <Line
+      id="weight_widget"
+      :options="chartOptions"
+      :data="chartData"
+    />
+  </template>
+  
+  <script>
+  import { Line } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
+  
+  ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
+  
+  export default {
+    name: 'weightWidget',
+    components: { Line },
+    data() {
+      return {
+        chartData: {
+        labels: ['16/03', '17/03', '18/03', '19/03', '20/03', '21/03', '22/03'],
+        datasets: [
+            {
+                backgroundColor: 'rgba(0, 128, 128, 0.5)', // Teal with some transparency for the point background
+            borderColor: '#008080',
+            data: [65.3, 64.5, 64, 65, 65.5, 65.2,65]
+            }
+        ]
+        },
+        chartOptions: {
+            responsive: true,
+            maintainAspectRatio: true,
+            plugins: {
+                legend: {
+                    display:false,
+                },
+                title: {
+                    display: true,
+                    text: 'Weight (kg)',
+                    color: 'white' // Adjust the title color
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white' // Adjust the x-axis labels color
+                    },
+                    grid: {
+                        display: false // Adjust the color of the x-axis grid lines
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: 'white' // Adjust the y-axis labels color
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.1)' // Adjust the color of the y-axis grid lines
+                    }
+                }
+            }
+        },
+        mounted() {
+            console.log('mount weight widget')
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#weight_widget {
+  background-color: #404b5a; /* Dark background color */
+  border-radius: 15px;      /* Rounded corners */
+  padding: 20px;            /* Padding around the chart */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Optional: Box shadow for depth */
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <h1>Home Page</h1>
+  <div>
+    <weightWidget />
+  </div>
 </template>
   
 <script>
-  
+  import weightWidget from '@/components/Weight.vue'
+
   export default {
     name: 'App',
     components: {
-
+      weightWidget
     },
     data() {
       return {
