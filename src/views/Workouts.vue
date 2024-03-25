8468c37570b848b718359e86186918e824132e07
davytan07 <45758052+davytan07@users.noreply.github.com>
2024-03-25 17:17:26 +0800

    Added Workout Pop-up to Workouts Page
    - Included a display button to test pop-up close button functionality
    - Added zoom hover animations for buttons
    - Adjusted scrolling and window resizing behaviour and appearance

--- src/views/Workouts.vue
+++ src/views/Workouts.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="container">
-    <workoutInfo />
+    <p @click="change" id="displaySample" v-if="!showWorkoutInfo">Display Sample Workout</p>
+    <transition name="fade">
+      <!-- Listen for the close event -->
+      <workoutInfo v-if="showWorkoutInfo" @close="showWorkoutInfo = false" />
+    </transition>
   </div>
 </template>
-  
-<script>
-  import WorkoutInfo from '@/components/WorkoutInfo.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      WorkoutInfo
-    },
-    data() {
-      return {
-        refreshComp: 0
-      }
-    },
-    methods: {
-      change() {
-        this.refreshComp += 1
-      }
+<script>
+import WorkoutInfo from '@/components/WorkoutInfo.vue';
+
+export default {
+  name: 'Workouts',
+  components: {
+    WorkoutInfo
+  },
+  data() {
+    return {
+      showWorkoutInfo: false
+    };
+  },
+  methods: {
+    change() {
+      this.showWorkoutInfo = !this.showWorkoutInfo;
     }
   }
+};
 </script>
 
-<style scoped>
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+<style scoped>  
+  #displaySample {
+    cursor: pointer;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.25s ease;
+  }
+
+  .fade-enter-from, .fade-leave-to {
+    opacity: 0;
   }
 </style>
