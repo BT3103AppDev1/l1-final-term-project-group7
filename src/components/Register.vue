6d1caac55fa80b939ac62bcb2ab9f5258cfd09a2
wesleychoy <39342100+wesleychoy@users.noreply.github.com>
2024-03-27 00:07:35 +0800

    seperated login + register pages final
    added login and register pages
    created these two components on its own for now to prevent conflicts
    access /login or /register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,58 +4,75 @@
       <h1 id="message">Create An Account</h1>
       <p><input type="text" placeholder="email" v-model="email" /></p>
       <p><input type="password" placeholder="password" v-model="password" /></p>
+      <p v-if="errorMessage">{{ errorMessage }}</p>
       <p><button @click="register">Submit</button></p>
       <p><button @click="signInWithGoogle">Sign In With Google</button></p>
       <h3 id="pagecontent">HealthGuru is a .... </h3>
     </div>
   </template>
-
 <script>
 import 'firebase/compat/auth';
 import { ref } from "vue";
-import { useRouter } from "vue-router";
+//import { useRouter } from "vue-router";
 import firebase from "@/uifire.js"
-import { getAuth } from 'firebase/auth';
-const auth = getAuth()
   
 export default {
-    setup() {
-        const email = ref("");
-        const password = ref("");
-        const router = useRouter();
+    data() {
+        return {
+            user: false,
+            email: "",
+            password: "",
+            errorMessage: "",
+        }
+    },
 
-        const register = () => {
-            firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    mounted() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log(user);
+                this.user = user;
+            }
+        })
+    },
+
+    methods: {
+        register() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
                 .then(() => {
                     console.log("Successfully registered!");
-                    router.push('/');
+                    //this.$router.push('/');
                 })
                 .catch((error) => {
                     console.error(error.code);
-                    alert(error.message);
+                    switch(error.code) {
+                        case "auth/email-already-in-use":
+                            this.errorMessage = "Email is in use";
+                            break;
+                        case "auth/weak-password)":
+                            this.errorMessage = "Password should have at least 6 characters";
+                        default:
+                            this.errorMessage = "Invalid email or password";
+                    }
+                    this.$forceUpdate();
                 });
-        };
+        },
 
-        const signInWithGoogle = () => {
+        signInWithGoogle() {
             const provider = new firebase.auth.GoogleAuthProvider();
             firebase.auth().signInWithPopup(provider)
                 .then((result) => {
                     console.log(result.user);
-                    router.push("/");
+                    this.$router.push("/");
                 })
                 .catch((error) => {
-                    console.error(error.code)
-                    alert(error.message)
+                    console.error(error.code);
+                    switch(error.code) {
+                        default:
+                            this.errorMessage = "Invalid email or password";
+                    }
+                    this.$forceUpdate();
                 });
         }
-
-
-        return {
-            email,
-            password,
-            register,
-            signInWithGoogle,
-        };
     }
 }
 </script>
