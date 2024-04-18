2875e2ba757a5c2eb34b667fe5886321fc14d0ac
wesleychoy <39342100+wesleychoy@users.noreply.github.com>
2024-04-18 15:36:19 +0800

    update register to options api, aligned with rest of project

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,53 +12,73 @@
 </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+<script>
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'firebase/auth';
 import firebaseApp  from '@/firebase.js'; 
+import { doc, getDoc, setDoc, getFirestore, updateDoc, arrayRemove, arrayUnion } from 'firebase/firestore';
+import { db } from '@/firebase';
 
-const email = ref('');
-const password = ref('');
-const errorMessage = ref('');
-const router = useRouter();
-const auth = getAuth(firebaseApp);
+export default {
+    name: 'Register',
 
-const register = async () => {
-    try {
-        await createUserWithEmailAndPassword(auth, email.value, password.value);
-        console.log("Successfully registered!");
-        router.push('/');
-    } catch (error) {
-        console.error(error.code);
-        switch(code) {
-            case "auth/email-already-in-use": 
-                errorMessage.value = "Email is in use";
-            case "auth/weak-password": 
-                errorMessage.value = "Password should have at least 6 characters";
-            default: 
-                errorMessage.value = "Invalid email or password";
+    data() {
+        return {
+            email: '',
+            password: '',
+            errorMessage: '',
+            user: '',
+            uid: '',
+            auth: getAuth(firebaseApp),
+        };
+    },
+
+    mounted() {
+        onAuthStateChanged(this.auth, (user) => {
+        if (user) {
+            this.user = user;
+            this.uid = user.uid;
         }
-    }
-}
+        })
+    },
 
+    methods: {
+        async register() {
+            try {
+                await createUserWithEmailAndPassword(this.auth, this.email, this.password);
+                console.log("Successfully registered!");
+                this.$router.push('/');
+            } catch (error) {
+                console.error(error.code);
+                switch(error.code) {
+                    case "auth/email-already-in-use": 
+                        this.errorMessage = "Email is in use";
+                        break;
+                    case "auth/weak-password": 
+                        this.errorMessage = "Password should have at least 6 characters";
+                        break;
+                    default: 
+                        this.errorMessage = "Invalid email or password";
+                }
+            }
+        },
 
-const registerWithGoogle = async () => {
-    try {
-        const provider = new GoogleAuthProvider();
-        await signInWithPopup(auth, provider);
-        console.log("Successfully registered with Google!");
-        router.push("/");
-    } catch (error) {
-        console.error(error.code);
-        errorMessage.value = "Failed to register with Google.";
-    }
-}
-      
-const goToLogin = async () => {
-    router.push('/login');
-}
+        async registerWithGoogle() {
+            try {
+                const provider = new GoogleAuthProvider();
+                await signInWithPopup(this.auth, provider);
+                console.log("Successfully registered with Google!");
+                this.$router.push('/');
+            } catch (error) {
+                console.error(error.code);
+                this.errorMessage = "Failed to register with Google.";
+            }
+        },
 
+        goToLogin() {
+            this.$router.push('/login');
+        }
+    },
+};
 </script>
 
 <style scoped>
